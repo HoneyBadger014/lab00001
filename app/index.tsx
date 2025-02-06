0a295e63a1b420e6bbe3ad928a857354b4820335
Aman <mnskkv@gmail.com>
2025-02-06 21:12:17 +0500

    Move snack code to repo

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Card } from 'react-native-paper';
+import Profile from './components/Profile';
+import Contacts from './components/Contacts';
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Profile />
+      <Contacts />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    padding: 8,
+  },
+});
